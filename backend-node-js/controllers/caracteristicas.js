2da0576e039c270396c82cdeaaa61771d0ea9577
Vinicio Borja <75345399+Vinici0@users.noreply.github.com>
2023-05-24 03:57:18 -0500

    Update caracteristicas.js

--- backend-node-js/controllers/caracteristicas.js
+++ backend-node-js/controllers/caracteristicas.js
@@ -1,19 +1,10 @@
 const { response, request } = require("express");
 const Caracteristica = require("../models/caracteristica");
 
-
-
 const getCaracteristicas = async (req = request, res = response) => {
   const caracteristicas = await Caracteristica.find();
   res.json(caracteristicas);
 }
-
-// const getCaracteristicasById = async (req = request, res = response) => {
-//   const { id } = req.params;
-//   const caracteristica = await Caracteristica.findById(id);
-//   res.json(caracteristica);
-// }
-
 
 const postCaracteristicas = async (req = request, res = response) => {
   //  //Color, referencia, ubicacion
@@ -30,22 +21,6 @@ const postCaracteristicas = async (req = request, res = response) => {
 
   res.status(201).json(caracteristica);
 }
-
-// const putCaracteristicas = async (req = request, res = response) => {
-
-//   const { id } = req.params;
-//   const { _id, ...resto } = req.body;
-//   const caracteristica = await Caracter
-//   caracteristica.findByIdAndUpdate(id, resto);
-//   res.json(caracteristica);
-// }
-
-// const deleteCaracteristicas = async (req = request, res = response) => {
-//   const { id } = req.params;
-//   const caracteristica = await Caracteristica.findByIdAndDelete(id);
-//   res.json(caracteristica);
-// }
-
 
 module.exports = {
   getCaracteristicas,
